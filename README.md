4865a3e58c541083c2ab2a6d37622851720e7f70
David Khuu <davidkhuu.dk@gmail.com>
2016-08-03 08:57:01 -0600

    Initial commit
    I’ve been trying to work on this for a few days and it’s still not
    fully working.

---
+++ README.md
@@ -0,0 +1,21 @@
+# Use the Twitch.tv JSON API
+```
+FreeCodeCamp
+|-- Front End Development Certification
+  |-- Intermediate Front End Development Projects
+    |-- Use the Twitchtv JSON API
+```
+
+## Objective
+Build a [CodePen.io](https://codepen.io/) app that is functionally similar to this: hhttps://codepen.io/FreeCodeCamp/full/Myvqmo/.
+
+## User Stories
+* I can see whether Free Code Camp is currently streaming on Twitch.tv.
+* I can click the status output and be sent directly to the Free Code Camp's Twitch.tv channel.
+* If a Twitch user is currently streaming, I can see additional details about what they are streaming.
+* I will see a placeholder notification if a streamer has closed their Twitch account (or the account never existed). You can verify this works by adding `brunofin` and `comster404` to your array of Twitch streamers.
+
+## Hints
+* See an example call to Twitch.tv's JSON API at https://github.com/FreeCodeCamp/FreeCodeCamp/wiki/Front-End-Project-Use-Twitchtv-JSON-API.
+* The relevant documentation about this API call is here: https://github.com/justintv/Twitch-API/blob/master/v3_resources/streams.md#get-streamschannel.
+* Here's an array of the Twitch.tv usernames of people who regularly stream: `["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]`
---
+++ main.js
@@ -0,0 +1,77 @@
+'use strict';
+
+let channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+
+let baseUrl = 'https://api.twitch.tv/kraken/channels/';
+let endUrl = '?callback=?';
+// make api call
+// receive JSON
+// parse JSON
+// for it for all channels in list
+
+function generateRow(streamer) {
+  var url = baseUrl + streamer + endUrl;
+
+  $.getJSON(url, function(data) {
+    console.log(data);
+
+    return buildHtml(data);
+  });
+}
+
+function buildHtml(stream) {
+  var channelName = stream.display_name,
+      currentGame = stream.game,
+      channelURL = stream.url,
+      channelLogo = stream.logo,
+      streamTitle = stream.status,
+      statusColour,
+      status;
+
+  // channel online
+  if (stream.mature) {
+    status = 'online';
+    statusColour = '#0a0';
+  } else {
+    status = 'offline';
+    statusColour = '#a00'
+  }
+
+  let $html = $('<div></div>');
+  $html.append(name);
+
+  let $row = $('<div></div>').addClass('row');
+  $row.attr('id', 'entry');
+
+  let $status = $('<div></div>').addClass('col-xs-1 col-md-1');
+  $status.attr('id', 'status');
+  $status.css('background-color', statusColour);
+
+  let $logo = $('<div></div>').addClass('col-xs-1 col-md-1');
+  let $logoImg = $('<img />');
+  $logo.attr('id', 'logo');
+  $logoImg.attr('src', channelLogo);
+  $logoImg.attr('id', 'stream-logo');
+  $logoImg.attr('width', '100%)');
+  $logo.append($logoImg);
+
+  let $info = $('<div></div>').addClass('col-xs-10 col-md-10');
+  $info.attr('id', 'info');
+  $info.append('<p>' + channelName + ' is playing ' + currentGame + '</p>');
+
+  $row.append([$status, $logo, $info]);
+
+  console.log(name);
+  $('.container').append($row);
+}
+
+$(document).ready(function() {
+  generateRow('summit1g');
+  generateRow('steel_tv');
+  generateRow('qosmonaut');
+  generateRow('phantoml0rd');
+  generateRow('shroud');
+  generateRow('hutch');
+});
+
+//getChannelInfo();
